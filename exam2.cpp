9256f4b9091b5cebdd6b43f62089efe63a16f7df
MAC PRO <imac@MACs-Mac-Pro.local>
2025-11-25 21:38:24 -0800

    EXAM 3 First push

---
+++ exam2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+ 
+using namespace std;
+
+
+int main() {
+
+// Arrays (1D) 
+
+
+// decclaring and initializing an array 
+int arr[6] = {20, 30, 39 , 40, 40, 40 }; 
+// now we acccess it with a for loop 
+for ( int i=0; i < 6; i++ ) { 
+    // printout the varlues inside 
+    cout << "Element at indix " << i  << " "  << arr[i] << endl; 
+}
+
+    int numbers[5] = {10, 20, 30, 40, 50}; // printed out 
+    for (int i = 0; i < 5; i++) {
+        cout << "Element at index " << i << ": " << numbers[i] << endl;
+    } 
+
+cout << "------------------------" << endl;
+// Another example
+int randomArray[20] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20,
+                        22, 24, 26, 28, 30, 32, 34, 36, 38, 20 };
+    // Accessinf elements using loops . 
+    for (int i = 0; i< 20 ; i++ ) { 
+        cout<< " Elemnt at index : " << i <<" " << randomArray[i] << endl; 
+    }
+
+cout << " ------------------------" << endl;
+
+cout << "\t another text \n"  ; 
+ 
+// declare and intializing arrays 
+int array2[5] = { 10 , 9 , 8 ,7 ,4};   // acceess the array with a for loop 
+for ( int i = 0; i < 5; i++ ) { 
+    cout << " Element at indix :" << i  <<  " "  << array2[i] << endl;
+}
+ 
+// how arrays behave wgeb oassed to functions ( pointers ) 
+// first we will create a function 
+
+void arrayPointer(int arr[], int size) { 
+
+    for ( int i = 0 ; i < size; i++ ) { 
+    cout << "\t Element at index : " << i << " " << arr[i] << endl;
+    }
+}
+
+
+
+int main () { 
+    int myArray[5] = { 1, 2, 3, 4, 5 };
+    arrayPointer(myArray, 5);
+}
+
+
+    return 0;
+}
